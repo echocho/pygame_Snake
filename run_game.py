63bb6c512ca0dfac0e9a713c99ca7f8c70b22808
echocho <echo333cheng@gmail.com>
2018-06-27 18:20:32 +0800

    Modified the way it ends

--- run_game.py
+++ run_game.py
@@ -1,4 +1,5 @@
 import pygame, random, time
+
 pygame.init()
 
 import snake_world
@@ -48,35 +49,32 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 clock = pygame.time.Clock()
 
 
-class BorderHitChecker:
-
-    def __init__(self):
-        self.status = False
-
-    def check(self):
-        (head_x_pos, head_y_pos) = Snake.head_location()
-        # print('head_x_pos, head_y_pos', head_x_pos, head_y_pos)
-        # print('border thickness', BORDER_THICKNESS)
-        if head_x_pos < BORDER_THICKNESS or head_x_pos > SCREEN_WIDTH - BORDER_THICKNESS or head_y_pos < BORDER_THICKNESS or head_y_pos > SCREEN_HEIGHT - BORDER_THICKNESS:
-            World.game_over(screen)
-            print('hit border')
-            self.status = True
+def hit_border():
+    (head_x_pos, head_y_pos) = Snake.head_location()
+    # print('head_x_pos, head_y_pos', head_x_pos, head_y_pos)
+    # print('border thickness', BORDER_THICKNESS)
+    if head_x_pos < BORDER_THICKNESS or head_x_pos > SCREEN_WIDTH - BORDER_THICKNESS or head_y_pos < BORDER_THICKNESS or head_y_pos > SCREEN_HEIGHT - BORDER_THICKNESS:
+        print('hit border')
+        return True
+    return False
 
 
-class CollapseChecker:
+def collapse_itself():
+    if Snake.body_list[-1] in Snake.body_list[:-1]:
+        print('Collapse with itself')
+        # print('Snake.bodyList', Snake.body_list)
+        # print(Snake.body_list[-1])
+        # print(Snake.body_list[:-1])
+        return True
+    return False
 
-    def __init__(self):
-        self.status = False
-
-    def check(self):
-        if Snake.body_list[-1] in Snake.body_list[:-1]:
-            World.game_over(screen)
-            print('Collapse with itself')
-            print('Snake.bodyList', Snake.body_list)
-            # print(Snake.body_list[-1])
-            # print(Snake.body_list[:-1])
-            self.status = True
-
+# -------------------- to be fixed
+def restart():
+    Manu.game_intro(screen, 'Snake of Simplicity', GAME_TITLE_SIZE)
+    if Manu.status_quit:
+        pygame.quit()
+    main_game_loop()
+# ------------------- to be fixed
 
 Manu.game_intro(screen, 'Snake of Simplicity', GAME_TITLE_SIZE)
 if Manu.status_quit:
@@ -117,30 +115,33 @@ def main_game_loop():
         # check if snake eats the food and then generate new food:
         food_x, food_y = Food.current_location()
         snake_x, snake_y = Snake.head_location()
-        print('(food_x, food_y)', (food_x, food_y))
-        print('(snake_x, snake_y)', (snake_x, snake_y))
-        print('Snake.body_list', Snake.body_list)
-        print('len of Snake.body_list', len(Snake.body_list))
+        # print('(food_x, food_y)', (food_x, food_y))
+        # print('(snake_x, snake_y)', (snake_x, snake_y))
+        # print('Snake.body_list', Snake.body_list)
+        # print('len of Snake.body_list', len(Snake.body_list))
 
         # if snake_x >= food_x and snake_x + SNAKE_RECT_LEN >= food_x + foodRectSize:
         if snake_x == food_x and snake_y == food_y:
-            print('----------------yes food in bodylist------------------')
+            # print('----------------yes food in bodylist------------------')
             Snake.extend_body(food_x, food_y)
             Food.generate_food(FOOD_RANGE_1, FOOD_RANGE_2)
             score += 1
-            print('--------------------------------------score------------------')
+            # print('--------------------------------------score------------------')
             print(score)
             World.score_cal(screen, score) # added
 
         Food.draw_food(screen)
 
-        BorderHitChecker().check()
-        if BorderHitChecker().status:
+
+# -------------------- to be fixed
+        if hit_border() or collapse_itself():
+            World.game_over(screen)
             carry_on = False
-        
-        CollapseChecker().check()
-        if CollapseChecker().status:
-            carry_on = False
+            restart()
+            print('carry_on is false now')
+
+# -------------------- to be fixed
+
 
         pygame.display.update()
         clock.tick(60)
